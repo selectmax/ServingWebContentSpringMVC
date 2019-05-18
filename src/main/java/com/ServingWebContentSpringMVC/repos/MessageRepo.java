0ba8e9a16834a806c2035714f0390a01d0884a38
Maltsev Maksim <Maksim.Maltsev@t-systems.ru>
2019-05-19 02:29:21 +0300

    filter

---
+++ src/main/java/com/ServingWebContentSpringMVC/repos/MessageRepo.java
@@ -0,0 +1,12 @@
+package com.ServingWebContentSpringMVC.repos;
+
+import com.ServingWebContentSpringMVC.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
+
+}
